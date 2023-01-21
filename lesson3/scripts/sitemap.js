5256e37e1d029484436c39b2c228f6b5c3f76983
Koigor97 <turaysamuel101@yahoo.com>
2023-01-21 22:52:46 +0200

    Finished with my Lynnwood Business Chamber website palnning document.

---
+++ lesson3/scripts/sitemap.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const date = new Date();
+const dateUpdate = document.querySelector("#date");
+
+dateUpdate.textContent = `${
+  date.getMonth() + 1
+}/${date.getDate()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
