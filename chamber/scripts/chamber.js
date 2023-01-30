4afd518beb4c0b0922fbfb97120be93f4975bb92
Koigor97 <turaysamuel101@yahoo.com>
2023-01-30 21:20:44 +0200

    built the responsive navigation, the footer, and the home page layout of the Lynnwood Chamber of Commerce site (Week-04).

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,35 @@
+"use strict";
+const button = document.querySelector(".mobile-nav");
+const primaryNav = document.querySelector(".primary-navigation");
+
+button.addEventListener('click', () => {
+    const isOpen = button.getAttribute('aria-expanded');
+    const isVisible = primaryNav.getAttribute("data-visible");
+    if (isOpen === 'false' && isVisible === 'false') {
+        button.setAttribute('aria-expanded', true);
+        primaryNav.setAttribute("data-visible", true)
+    } else {
+        button.setAttribute('aria-expanded', false);
+        primaryNav.setAttribute("data-visible", false);
+    }
+})
+
+const date = new Date();
+const dateUpdate = document.querySelector("#date");
+const topDate = document.querySelector("#top-date");
+
+dateUpdate.textContent = `${
+  date.getMonth() + 1
+}/${date.getDate()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+try {
+    const options = {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        year: "numeric"
+    };
+    topDate.textContent = `${date.toLocaleDateString("en-SA", options)}`;
+} catch (e) {
+    console.log("Error with code or your browser does not support Locale");
+}
